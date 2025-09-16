3c260c60675b6c02ec31ee5fb4c26b215c2b5706
andrepasion04-bit <andrepasion04@gmail.com>
2025-09-15 23:17:34 -0500

    update: added read file and character count functions

---
+++ Assignment_1/text_stats.py
@@ -0,0 +1,20 @@
+# read data the intput file (named input,txt)
+# read it into a variable
+
+# process (parse) (clean it!) (get it ready for...???) -analysis
+# remove the punctuation and extra whitspace \n \t \s
+# normalization (lowercase the text)
+# make it so we can compare our data
+# tokenization (split the string into it's component parts)
+# .split() \ - (returns a list of strings)
+
+
+# calculate/accumulate statistics
+# setup varaibles/data structures for our data
+# Len() - will give us the word count and char
+# set() - will give unique word count 
+# avarage word count 
+
+
+# output the stats to the terminal and a file (output.txt)
+# (in the presribed format)
