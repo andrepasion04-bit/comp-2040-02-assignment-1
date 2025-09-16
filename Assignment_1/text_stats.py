fe03d5d9f1d97566fcc6ec84757b0ec121ec950d
andrepasion04-bit <andrepasion04@gmail.com>
2025-09-15 23:31:49 -0500

    Update: added missing files

--- Assignment_1/text_stats.py
+++ Assignment_1/text_stats.py
@@ -1,11 +1,23 @@
 # read data the intput file (named input,txt)
+
+def read_file(file_path):
+    '''Reads the content of a file and returns it as a string.'''
+    with open(file_path, 'r') as file:
+        return file.read()
+
 # read it into a variable
+text = read_file('input_1.txt')
 
 # process (parse) (clean it!) (get it ready for...???) -analysis
+
 # remove the punctuation and extra whitspace \n \t \s
+
 # normalization (lowercase the text)
+
 # make it so we can compare our data
+
 # tokenization (split the string into it's component parts)
+
 # .split() \ - (returns a list of strings)
 
 
@@ -18,3 +30,14 @@
 
 # output the stats to the terminal and a file (output.txt)
 # (in the presribed format)
+
+
+def character_count_with_spaces(text): 
+    return len(text)
+
+def character_count_no_spaces(text):
+    return len(text.replace(" ", "").replace("\n", "").replace("\t", ""))
+
+
+
+print(character_count_no_spaces(text))
